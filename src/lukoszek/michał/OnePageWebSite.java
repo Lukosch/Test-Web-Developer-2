b019570484a7a8e19a00a49dd85fd2096af5f3cc
Michał <michal.lukoszek@icloud.com>
2018-06-21 15:54:44 +0200

    0.0.1
    Block added in the method doPost and doGet.
    Added index.jsp

--- src/lukoszek/michał/OnePageWebSite.java
+++ src/lukoszek/michał/OnePageWebSite.java
@@ -7,13 +7,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/home")
+@WebServlet(urlPatterns = {"/home", "*.do"})
 public class OnePageWebSite extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String name = request.getParameter("name");
+        if (name != null && name != "") {
+            response.getWriter().printf("Hello %s", name);
+        } else {
+            response.sendRedirect("index.jsp");
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().write("Hello World");
+        String name = request.getParameter("name");
+        if (name != null) {
+            response.getWriter().printf("Hello %s", name);
+        } else {
+            response.getWriter().write("Please enter a name");
+        }
     }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -1,16 +1,23 @@
 <%--
   Created by IntelliJ IDEA.
   User: Szmaragdowy
-  Date: 20.06.2018
-  Time: 17:49
+  Date: 21.06.2018
+  Time: 12:16
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <form action="home" method="post">
+        <p>
+            Name: <input type="text" name="name">
+        </p>
+        <p>
+            <input type="submit" value="Enter name">
+        </p>
+    </form>
+</body>
 </html>
